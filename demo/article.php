f4606852dd4a5285f4c708bc04beaea21208dca5
mateuszdolega <mdx@mdx.pl>
2015-03-02 00:22:34 +0100

    new WebnalistFrontend embed code

--- demo/article.php
+++ demo/article.php
@@ -58,32 +58,15 @@ if ($isPurchased && !$error) {
 echo $view;
 
 ?>
-
-<script>
-    (function (d, s, wn) {
-        window['WN'] = wn;
-        wn.readyFn = wn.readyFn || [];
-        wn.ready = function (fn) {
-            wn.readyFn.push(fn);
-        };
-        var wns = d.createElement(s);
-        wns.async = true;
-        wns.src = 'js/webnalist.min.js'; //Make sure the path is correct.
-        wns.onload = function () {
-            wn.executeReady && wn.executeReady();
-        };
-        var tag = document.getElementsByTagName("script")[0];
-        tag.parentNode.insertBefore(wns, tag);
-    })(document, 'script', window['WN'] || {});
-</script>
-<script>
-    WN.options = {
-        loadPrices: false,
-        <?php if(SANDBOX_MODE) : ?>
-        readArticleUrl: '<?php echo PAGE_URL ?>/sandbox/confirm.php', //only for sandbox, remove this on production mode
-        loadPricesUrl: '<?php echo PAGE_URL ?>/sandbox/prices.php' //only for sandbox, remove this on production mode
-        <?php endif; ?>
-    };
-</script>
+<script src="path/to/webnalist.min.js"></script>
+<?php if (SANDBOX_MODE) : ?>
+    <script>
+        WN.options = {
+            sandbox: {
+                url: '<?php echo PAGE_URL ?>'
+            }
+        }
+    </script>
+<?php endif; ?>
 </body>
 </html>
--- demo/index.php
+++ demo/index.php
@@ -9,7 +9,8 @@
 
 <h1>Webnalist Merchant Demo Store</h1>
 <ul>
-    <li class="wn-item" data-wn-url="http://demo.webnalist.com/article.php?id=1<?php if(SANDBOX_MODE):?>#purchased<?php endif; ?>">
+    <li class="wn-item"
+        data-wn-url="http://demo.webnalist.com/article.php?id=1<?php if (SANDBOX_MODE): ?>#purchased<?php endif; ?>">
         <a href="#">
             Purchased Article #1 Title <em>Price: <span class="wn-price">...</span> zł</em>
         </a>
@@ -29,31 +30,17 @@
 <a href="#" class="wn-item" data-wn-url="http://demo.webnalist.com/article.php?id=1">Or simple try read
     article #1 &raquo;</a>
 
-<script>
-    (function (d, s, wn) {
-        window['WN'] = wn;
-        wn.readyFn = wn.readyFn || [];
-        wn.ready = function (fn) {
-            wn.readyFn.push(fn);
-        };
-        var wns = d.createElement(s);
-        wns.async = true;
-        wns.src = 'js/webnalist.min.js'; //Make sure the path is correct.
-        wns.onload = function () {
-            wn.executeReady && wn.executeReady();
-        };
-        var tag = document.getElementsByTagName("script")[0];
-        tag.parentNode.insertBefore(wns, tag);
-    })(document, 'script', window['WN'] || {});
-</script>
-<script>
-    WN.options = {
-        loadPrices: true, //load prices
-        <?php if(SANDBOX_MODE) : ?>
-        readArticleUrl: '<?php echo PAGE_URL ?>/sandbox/confirm.php', //only for sandbox, remove this on production mode
-        loadPricesUrl: '<?php echo PAGE_URL ?>/sandbox/prices.php' //only for sandbox, remove this on production mode
-        <?php endif; ?>
-    };
-</script>
+
+<script src="path/to/webnalist.min.js"></script>
+<?php if (SANDBOX_MODE) : ?>
+    <script>
+        WN.options = {
+            loadPrices: true,
+            sandbox: {
+                url: '<?php echo PAGE_URL ?>'
+            }
+        }
+    </script>
+<?php endif; ?>
 </body>
 </html>
