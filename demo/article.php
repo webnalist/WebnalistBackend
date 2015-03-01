6dd3e37cfd2caa3d2c7542fe135bef96afac058e
mateuszdolega <mdx@mdx.pl>
2015-03-01 03:27:44 +0100

    demo js path fixed

--- demo/article.php
+++ demo/article.php
@@ -7,19 +7,7 @@
 <body>
 
 <?php
-define('PAGE_URL', 'http://demo.webnalist.com'); //your demo page host, skip / on the end
-define('WN_KEY_PUBLIC', 'xxx'); //your webnalist brand public key
-define('WN_KEY_PRIVATE', 'xxx'); //your webnalist brand private key
-
-/**
- * You can test without adding articles to Webnalist database.
- * If you add at the end of the article url #purchased and call in sandbox mode,
- * we send to your service purchased response in other case you receive unpaid response.
- *
- * In production mode hash on the end of url is ignored.
- */
-define('SANDBOX_MODE', true);
-
+include_once('conf.php');
 include_once('../../WebnalistBackend.php');
 
 //demo data
--- demo/index.php
+++ demo/index.php
@@ -1,3 +1,4 @@
+<?php include_once('conf.php'); ?>
 <!DOCTYPE html>
 <html>
 <head lang="en">
