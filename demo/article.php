b78c69dbdcfb81a7485c63910e6c20dc4c5779e2
mateuszdolega <mdx@mdx.pl>
2015-03-01 05:08:43 +0100

    wn-read => wn-item class name on article page

--- demo/article.php
+++ demo/article.php
@@ -19,7 +19,7 @@ $lead = '<p>Lead for article #' . $articleId . ' Eu leo sem velit, odio nam ipsu
 $fullText = '<hr><p><strong>Full text of article #' . $articleId . '</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
 <h2>Header Level 2</h2><ol><li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li><li>Aliquam tincidunt mauris eu risus.</li></ol>';
 $purchasedInfo = '<p><strong>Artykuł kupiony przez <a href="https://webnalist.com" target="_blank">Webnalist.com</a></strong></p>';
-$purchaseUrl = sprintf('<p><a href="#" data-wn-url="%s" class="wn-read">Przeczytaj całość z Webnalist.com</a></p>',
+$purchaseUrl = sprintf('<p><a href="#" data-wn-url="%s" class="wn-item">Przeczytaj całość z Webnalist.com</a></p>',
     urlencode($currentArticleUrl));
 
 //Inicjowanie usługi Webnalist w trybie sandbox i debug
